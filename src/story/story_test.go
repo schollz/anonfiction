096506968e1c8552435573541191070b3f98a412
Zack Scholl <zack.scholl@gmail.com>
2017-12-19 06:11:33 -0700

    Fix tests

--- src/story/story_test.go
+++ src/story/story_test.go
@@ -4,7 +4,6 @@ import (
 	"html/template"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/schollz/anonfiction/src/user"
 	"github.com/stretchr/testify/assert"
@@ -15,47 +14,13 @@ func TestStory(t *testing.T) {
 	os.Remove("story_test.db")
 	user.DB = "user_test.db"
 	DB = "story_test.db"
-	err := user.Add("zack", "123", "english", false)
+	err := user.Add("zack",  "english", false)
 	assert.Nil(t, err)
 
-	apikey, err := user.Validate("zack", "123")
-	assert.Nil(t, err)
-	_, err = Update("story0", apikey, "being", "to be or not to be, that is the question", []string{"questions"}, false)
-	assert.Nil(t, err)
-
-	content, err := GetStory("story0", apikey)
-	assert.Nil(t, err)
-	assert.Equal(t, "to be or not to be, that is the question", content)
-
-	_, err = Update("story0", apikey, "being", "to be or not to be, that is the question?", []string{"questions"}, false)
-	assert.Nil(t, err)
-
-	content, err = GetStory("story0", apikey)
-	assert.Nil(t, err)
-	assert.Equal(t, "to be or not to be, that is the question?", content)
-
-	_, err = Update("asdfasdf", "incorrectapikey", "being", "to be or not to be, that is the question", []string{"questions"}, false)
-	assert.NotNil(t, err)
-
-	time.Sleep(1)
-	Update("story1", apikey, "being", "a new story", []string{"questions"}, false)
-	user.Add("zack2", "123", "english", false)
-	apikey2, _ := user.Validate("zack2", "123")
-	Update("anotherstory", apikey2, "being", "a new story", []string{"questions"}, false)
-	stories, err := ListByUser(apikey)
-	assert.Nil(t, err)
-	assert.Equal(t, 2, len(stories))
-	assert.True(t, stories[0].Date.After(stories[1].Date))
-	stories, err = ListByUser(apikey2)
-	assert.Nil(t, err)
-	assert.Equal(t, 1, len(stories))
-	stories, err = ListByTopic("being")
-	assert.Nil(t, err)
-	assert.Equal(t, 3, len(stories))
 
 	s := "<div>This is the first paragraph.<br><br>This has two lines.<br>But it should be one paragraph.<br><br>This&nbsp;<em>has italic</em>.&nbsp;<strong>This</strong> is bold. This is the third paragraph.</div>"
 	ss := ConvertTrix(s)
-	assert.Equal(t, template.HTML("<span class=\"leader f4\">This is the first paragraph."), ss[0])
+	assert.Equal(t, template.HTML("<span class=\"startsentence\">This is the first paragraph."), ss[0])
 	assert.Equal(t, template.HTML("This has two lines. But it should be one paragraph."), ss[1])
 	assert.Equal(t, template.HTML("This&nbsp;<em>has italic</em>.&nbsp;<strong>This</strong> is bold. This is the third paragraph."), ss[2])
 }
--- src/user/user_test.go
+++ src/user/user_test.go
@@ -10,33 +10,8 @@ import (
 func TestUser(t *testing.T) {
 	os.Remove("user_test.db")
 	DB = "user_test.db"
-	err := Add("zack", "pass123", "english", false)
+	err := Add("zack", "english", false)
 	assert.Nil(t, err)
-	err = Add("zack", "pass123", "english", false)
+	err = Add("zack", "english", false)
 	assert.NotNil(t, err)
-	var apikey string
-	apikey, err = Validate("zack", "pass123")
-	assert.Nil(t, err)
-	assert.Equal(t, 10, len(apikey))
-	apikey, err = Validate("zack", "pass122")
-	assert.NotNil(t, err)
-	apikey, err = Validate("zafck", "pass122")
-	assert.NotNil(t, err)
-
-	// test admin rights
-	Add("jlkjklj", "pass123", "english", false)
-	u, _ := Get("jlkjklj")
-	assert.Equal(t, false, u.IsAdmin)
-	err = SetAdmin("jlkjklj", true)
-	assert.Nil(t, err)
-	u, _ = Get("jlkjklj")
-	assert.Equal(t, true, u.IsAdmin)
-
-	// test re-adding
-	Add("bob1", "pass123", "english", false)
-	u, _ = Get("bob1")
-	err = Add("bob1", "pass234", "english", false)
-	assert.NotNil(t, err)
-	u2, _ := Get("bob1")
-	assert.Equal(t, u2.PasswordHash, u.PasswordHash)
 }
